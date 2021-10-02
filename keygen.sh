16e799786f34721e646e14a2b7e7ab3816137f1a
davidovski <david@davidovski.xyz>
2021-10-02 23:21:53 +0100

    added packages

--- keygen.sh
+++ keygen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p keychain
-openssl genrsa -out keychain/xi.pem 4096
-openssl rsa -in keychain/xi.pem -pubout > keychain/xi.pub
+openssl genrsa -out keychain/psi.pem 4096
+openssl rsa -in keychain/psi.pem -pubout > keychain/psi.pub
